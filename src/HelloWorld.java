485eccfa85bf1f225c750c256a7274f979c3d70f
harshinimitta <sivaharshinimitta@gmail.com>
2017-05-04 10:55:08 -0400

    Added my favorite musical genre

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,8 +3,9 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         System.out.println("Hello Conflicts from Sandbox!");
-        System.out.println("My favourite Band is OneRepublic");
-        System.out.println("My favourite album of OneRepublic is Native");
+        System.out.println("My favorite Band is OneRepublic");
+        System.out.println("My favorite album of OneRepublic is Native");
+        System.out.println("My favorite musical genre is Pop");
     }
 
 }
