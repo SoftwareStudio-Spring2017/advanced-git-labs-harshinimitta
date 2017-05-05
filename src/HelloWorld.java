c5af8a41da5ee45334db0ffd64323ee8fe0d1d57
harshinimitta <sivaharshinimitta@gmail.com>
2017-05-04 22:11:13 -0400

    Added a print statement to verify stashing

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -7,6 +7,7 @@ public class HelloWorld {
         System.out.println("My favorite album of OneRepublic is Native");
         System.out.println("My favorite musical genre is Pop");
         System.out.println("Added as part of stashing in lab #11");
+        System.out.println("Rechecking the stash process");
     }
 
 }
